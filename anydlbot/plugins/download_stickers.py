057834abdff3b0e6ab00d98bad1f011bab8661e0
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-02 17:07:40 +0530

    Update download_stickers.py

--- anydlbot/plugins/download_stickers.py
+++ anydlbot/plugins/download_stickers.py
@@ -9,7 +9,7 @@ import time
 from PIL import Image
 from pyrogram import (
     Client,
-    Filters
+    filters
 )
 from anydlbot import (
     AUTH_USERS,
@@ -27,7 +27,7 @@ LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@Client.on_message(Filters.sticker)
+@Client.on_message(filters.sticker)
 async def DownloadStickersBot(bot, update):
     if update.from_user.id not in AUTH_USERS:
         await update.delete()

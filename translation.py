187578312d9c6af10645b65fa13188bb3274b741
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-02 13:42:28 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,66 @@
-class Translation:
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
-Source Code: https://github.com/SpEcHiDe/AnyDLBot
-© @AnyDLBot"""
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+class Translation(object):
+
+    START_TEXT = """
+<b>Hey</b><b> {} </b>
+
+<b>I am Telegram Most Powerful Url Uploader Bot</b>
+
+<b>I can Upload Any Link in File or Video</b>
+
+<b>Use Help Command to Know How to Use me</b>
+
+<b>Made With 💕 By</b><b> @Tellybots_4u</b>
+"""
+    HELP_TEXT = """
+<b>Link to Media or File</b>
+➠ <b>Send a link for upload to telegram file or media.</b>
+
+<b>Set Thumbnail</b>
+➠ <b>Send a photo to make it as permanent thumbnail.</b>
+
+<b>Deleting Thumbnail</b>
+➠ <b>Send /delthumb to delete thumbnail.</b>
+
+<b>Show Thumbnail</b>
+➠ <b>Send /showthumb to view custom thumbnail.</b>
+
+<b>Made With 💕 By</b><b> @Tellybots_4u</b>
+"""
+    ABOUT_TEXT = """
+ **🤖 <b>Bot :** URL Uploader</b>\n
+ **👲 <b>Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)</b>\n
+ **👥 <b>Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)</b>\n
+ **❄️ <b>Credits :** Everyone in this journey</b>\n
+ **🍴 <b>Source :** [Click here](https://t.me/tellybots_digital)</b>\n
+ **📝 <b>Language :** [Python3](https://python.org)</b>\n
+ **📚 <b>Library :** [Pyrogram v1.2.0](https://pyrogram.org)</b>\n
+ **🌟 <b>Server :** [Heroku](https://heroku.com)</b>\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "This is an @AnyDLBot clone created using the Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nStart @AnyDLBot to know Paid Plans!"

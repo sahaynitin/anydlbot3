0c810b5149bda41836c7a518b1dc4e3a548567ec
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-02 17:49:35 +0530

    Update youtube_dl_echo.py

--- anydlbot/plugins/youtube_dl_echo.py
+++ anydlbot/plugins/youtube_dl_echo.py
@@ -101,8 +101,13 @@ async def echo(bot, update):
     if youtube_dl_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(youtube_dl_password)
-    # logger.info(command_to_exec)
-    # logger.info(command_to_exec)
+        # logger.info(command_to_exec)
+    process = await asyncio.create_subprocess_exec(
+        *command_to_exec,
+        # stdout must a pipe to be accessible as process.stdout
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
     t_response, e_response = await run_shell_command(command_to_exec)
     # https://github.com/rg3/youtube-dl/issues/2630#issuecomment-38635239
     if e_response and "nonnumeric port" not in e_response:

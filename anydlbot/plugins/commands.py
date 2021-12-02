312cff9a12d3ec62dfb7d528e40346ab8ce177d0
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-02 14:05:34 +0530

    Update commands.py

--- anydlbot/plugins/commands.py
+++ anydlbot/plugins/commands.py
@@ -2,7 +2,7 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from anydlbot.sample_config import Config
 else:
-    from sample_config import Config
+    from anydlbot.sample_config import Config
 from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
